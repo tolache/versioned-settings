4caeea752be2cf2b2ae3f80b9eebbe23876fb8a7
Admin Adminson <Admin Adminson@TeamCity>
2020-02-25 11:35:59 +0300

    TeamCity change in 'DSL Test' project: Versioned settings configuration updated

---
+++ .teamcity/DslTest/Project.kt
@@ -0,0 +1,31 @@
+package DslTest
+
+import DslTest.buildTypes.*
+import DslTest.vcsRoots.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.versionedSettings
+
+object Project : Project({
+    uuid = "8bf25bd0-ac48-464e-8c39-593deb5dba51"
+    id("DslTest")
+    parentId("_Root")
+    name = "DSL Test"
+
+    vcsRoot(DslTest_RepoA)
+
+    buildType(DslTest_BuildA)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_16"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
+            rootExtId = "VersionedSettings"
+            showChanges = false
+            settingsFormat = VersionedSettings.Format.KOTLIN
+            storeSecureParamsOutsideOfVcs = true
+        }
+    }
+})
