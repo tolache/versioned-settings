3552cef6b946264941696ced018f65b42cf137d8
tolache <anatoly.cherenkov@gmail.com>
2020-02-25 11:34:09 +0300

    remove settings

--- .teamcity/_Self/buildTypes/MaximumNumberOfBuilds.kt
+++
@@ -1,9 +0,0 @@
-package _Self.buildTypes
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
-object MaximumNumberOfBuilds : Template({
-    name = "maximumNumberOfBuilds"
-
-    maxRunningBuilds = 10
-})
