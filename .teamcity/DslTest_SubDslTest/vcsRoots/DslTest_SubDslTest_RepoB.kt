8559bbed71a46f0b7aded70b37ca3980f629ba91
Admin Adminson <Admin Adminson@TeamCity>
2020-02-25 12:22:32 +0300

    TeamCity change in 'DSL Test' project: Synchronization with own VCS root is enabled

--- .teamcity/DslTest_SubDslTest/vcsRoots/DslTest_SubDslTest_RepoB.kt
+++ .teamcity/DslTest_SubDslTest/vcsRoots/DslTest_SubDslTest_RepoB.kt
@@ -1,12 +1,12 @@
-package DslTest.vcsRoots
+package DslTest_SubDslTest.vcsRoots
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-object DslTest_RepoA : GitVcsRoot({
-    uuid = "7aa20e42-a39a-4eae-9f36-22cf4e1f01ec"
-    name = "repoA"
-    url = "https://github.com/tolache/repoA"
+object DslTest_SubDslTest_RepoB : GitVcsRoot({
+    uuid = "4fb3c5d4-6a04-4c5a-bfb8-bab4eada7164"
+    name = "repoB"
+    url = "https://github.com/tolache/repoB"
     authMethod = password {
         userName = "tolache"
         password = "credentialsJSON:7d8cca8e-bc35-4156-a965-0b32123691bc"
