617403e64c0ba81d0e1cebb61c36019d6aef8be7
Anatoly Cherenkov <32959649+tolache@users.noreply.github.com>
2020-06-30 17:36:09 +0300

    fix the settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -24,7 +24,6 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 (Plugins -> teamcity-configs -> teamcity-configs:generate), the
 'Debug' option is available in the context menu for the task.
 */
-INTENTIONALLY INCORRECT SETTING
 
 version = "2019.2"
 
